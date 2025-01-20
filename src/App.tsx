94f52170b97cf8c224cb02665d751e8abb7d3b96
gurkirxt <gurkirat4874@gmail.com>
2025-01-20 22:27:05 +0530

    added Sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { useState } from "react";
 import { invoke } from "@tauri-apps/api/core";
 import "./input.css";
-import { Button } from "@/components/ui/button"
-
+import Sidebar from "@/components/page/sidebar"
 
 function App() {
 
   return (
     <>
-      <Button variant="secondary" className="bg-red-50">Secondary</Button>
+      <Sidebar></Sidebar>
     </>
   );
 }

533abba82682245facb7481500312bd2bd4bcfc4
ajiteshchandel <ajiteshchandel121@gmail.com>
2025-02-03 22:13:42 +0530

    fixed top bar issue

--- src/App.tsx
+++ src/App.tsx
@@ -101,7 +101,7 @@ function App() {
             onFileClick={handleFileClick}
           />
           <SidebarInset>
-            <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4 ">
+            <header className="sticky top-0 z-50 bg-background flex h-16 shrink-0 items-center gap-2 border-b px-4 ">
               <SidebarTrigger className="-ml-1" />
               <Separator orientation="vertical" className="mr-2 h-4" />
               <Breadcrumb>
